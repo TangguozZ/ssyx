bd9476eb37870c14782e7a8c2dce5eeaba39f01b
tangzhiqiang <tangzhiqiang@yuanbaobaoxian.com>
2023-06-26 22:58:29 +0800

    ADD: 初始化项目

---
+++ common/service-util/src/main/java/com/atguigu/ssyx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.atguigu.ssyx.common.exception;
+
+import com.atguigu.ssyx.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author：tangzhiqiang
+ * @Date：2023/6/26 0:06
+ * @Filename：GlobalExceptionHandler
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+}
---
+++ common/service-util/src/main/java/com/atguigu/ssyx/common/exception/SsyxException.java
@@ -0,0 +1,42 @@
+package com.atguigu.ssyx.common.exception;
+
+import com.atguigu.ssyx.common.result.ResultCodeEnum;
+import lombok.Data;
+
+/**
+ * @Author：tangzhiqiang
+ * @Date：2023/6/26 0:08
+ * @Filename：SsyxException
+ */
+@Data
+public class SsyxException extends RuntimeException{
+    //异常状态码
+    private Integer code;
+
+    /**
+     * 通过状态码和错误消息创建异常对象
+     * @param message
+     * @param code
+     */
+    public SsyxException(String message, Integer code) {
+        super(message);
+        this.code = code;
+    }
+
+    /**
+     * 接收枚举类型对象
+     * @param resultCodeEnum
+     */
+    public SsyxException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "GuliException{" +
+                "code=" + code +
+                ", message=" + this.getMessage() +
+                '}';
+    }
+}
