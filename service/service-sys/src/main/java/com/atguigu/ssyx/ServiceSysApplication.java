27ffcb873ab9a34c228eccaf154521d38b001d78
tangzhiqiang <tangzhiqiang@yuanbaobaoxian.com>
2023-07-01 17:09:37 +0800

    MOD: 到开通区域的基础的crud

---
+++ service/service-sys/src/main/java/com/atguigu/ssyx/ServiceSysApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.ssyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：tangzhiqiang
+ * @Date：2023/7/1 17:02
+ * @Filename：ServiceSysApplication
+ */
+@SpringBootApplication
+public class ServiceSysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSysApplication.class, args);
+    }
+}
